108b273d1685fe90bab7ce2b0fa16d93205c20c9
Mikel Gascon <mikelgascon@outlook.com>
2020-06-30 16:55:27 -0400

    add files for all the pieces

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import Pawn from 'components/pieces/Pawn';
+import Rook from 'components/pieces/Rook';
+import Queen from 'components/pieces/Queen';
+import King from 'components/pieces/King';
+import Bishop from 'components/pieces/Bishop';
+import Knight from 'components/pieces/Knight';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="board">
+      <Rook />
+      <Knight />
+      <Bishop />
+      <Queen />
+      <King />
+      <Bishop />
+      <Knight />
+      <Rook />
+
+      <Pawn />
     </div>
   );
 }
---
+++ client/src/components/pieces/King.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ReactComponent as KingImage } from 'images/king.svg';
+
+export default function King() {
+  return <KingImage className="piece" />;
+}

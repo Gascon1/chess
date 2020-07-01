c06454354309cd7009a868af593eeeed5a1b8e17
Bobae <bobae.choi2@gmail.com>
2020-07-01 16:14:33 -0700

    add all piece images to the board (no functionality added)

--- client/src/components/pieces/King.tsx
+++ client/src/components/pieces/King.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as KingImage } from 'images/king.svg';
 
-export default function King() {
-  return <KingImage className="piece" />;
+// export default function King() {
+//   return <KingImage className="piece" />;
+// }
+
+interface Position {
+  tile: string;
+  x: number;
+  y: number;
+  isOccupied?: boolean;
+  isFriendly?: boolean;
+}
+
+interface Props {
+  white: boolean;
+  tileInfo: Position;
+}
+
+export default function King(props: Props) {
+  const { tileInfo, white } = props;
+  const [state, setState] = useState({
+    hasUsedFirstMoved: false,
+    isWhite: true,
+    currentPosition: {
+      tile: '',
+      x: 0,
+      y: 0,
+    },
+  });
+
+  useEffect(() => {
+    setState((prev) => ({ ...prev, isWhite: white, currentPosition: tileInfo }));
+  }, [tileInfo, white]);
+  return <KingImage className={`piece ${props.white ? 'white' : 'black'}`} />;
 }
--- client/src/components/pieces/Queen.tsx
+++ client/src/components/pieces/Queen.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as QueenImage } from 'images/queen.svg';
 
-export default function Queen() {
-  return <QueenImage className="piece" />;
+// export default function Queen() {
+//   return <QueenImage className="piece" />;
+// }
+
+interface Position {
+  tile: string;
+  x: number;
+  y: number;
+  isOccupied?: boolean;
+  isFriendly?: boolean;
+}
+
+interface Props {
+  white: boolean;
+  tileInfo: Position;
+}
+
+export default function Queen(props: Props) {
+  const { tileInfo, white } = props;
+  const [state, setState] = useState({
+    hasUsedFirstMoved: false,
+    isWhite: true,
+    currentPosition: {
+      tile: '',
+      x: 0,
+      y: 0,
+    },
+  });
+
+  useEffect(() => {
+    setState((prev) => ({ ...prev, isWhite: white, currentPosition: tileInfo }));
+  }, [tileInfo, white]);
+  return <QueenImage className={`piece ${props.white ? 'white' : 'black'}`} />;
 }
